c4e6e36cb67ff2ef5a996a2e23afbe9a77ff7c8e
NateTheCarrot <34255388+NateTheCarrot@users.noreply.github.com>
2020-12-15 15:22:41 -0500

    Worked on database a bit. Currently trying to insert if doesn't exist already.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import mysql.connector
 with open('./storage/config.json') as f:
     config = json.load(f)
 
+prefix = config.get("prefix")
+
 mydb = mysql.connector.connect(
   host=config.get("db_host"),
   user=config.get("db_user"),
@@ -16,9 +18,6 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE TABLE IF NOT EXISTS allowed_channels (id TINYTEXT, allowed BOOL);") # TINYTEXT to make sure there are no incompatibilities with types.
-mycursor.execute("CREATE TABLE IF NOT EXISTS lines (sentences TEXT, replies LONGTEXT);") # Currently not working. Program thinks "lines" is part of the "CREATE TABLE".
-
 myresult = mycursor.fetchone()
 
 client = discord.Client()
@@ -26,6 +25,21 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('Currently online!')
+@client.event
 async def on_message(message):
     msg = message.content.lower()
+    if(msg == prefix + "add"):
+        #with open('./storage/addchannel.sql') as f:
+            #cursor.execute(f.read().decode('utf-8'), multi=True) # More efficient to do this with a file rather than a line.
+        sql = "INSERT IGNORE INTO allowed_channels SET (channel_id, allowed) VALUES (%s, %s);"
+        val = (str(message.channel.id), 1)
+        mycursor.execute(sql, val, multi=True)
+        mydb.commit()
+        await message.channel.send("Successfully made this channel a conversating channel.")
+        return
+    if(msg == prefix + "remove"):
+        mycursor.execute("UPDATE allowed_channels SET allowed = 0 WHERE channel_id = " + str(message.channel.id))
+        mydb.commit()
+        await message.channel.send("Successfully made this a non-conversating channel.")
+        return
 client.run(config.get("token"))
