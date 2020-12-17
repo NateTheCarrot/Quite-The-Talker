15234c20c2f3555abbeacd6bcb486f959f6c3fcd
NateTheCarrot <34255388+NateTheCarrot@users.noreply.github.com>
2020-12-17 14:37:47 -0500

    Finished base bot. Will fix bugs if needed, but everything should work finally.

--- main.py
+++ main.py
@@ -21,25 +21,84 @@ mycursor = mydb.cursor()
 myresult = mycursor.fetchone()
 
 client = discord.Client()
-
+def filter_message(orig):
+    with open('./storage/banned_words.txt', "r") as f:
+        banned_words = f.read().split(";")
+        filtered = orig
+        for i in range(len(banned_words)):
+            filtered = filtered.replace(banned_words[i], "")
+    return filtered
 @client.event
 async def on_ready():
     print('Currently online!')
 @client.event
 async def on_message(message):
+    if(message.author.bot):
+        return
     msg = message.content.lower()
     if(msg == prefix + "add"):
-        #with open('./storage/addchannel.sql') as f:
-            #cursor.execute(f.read().decode('utf-8'), multi=True) # More efficient to do this with a file rather than a line.
-        sql = "INSERT IGNORE INTO allowed_channels SET (channel_id, allowed) VALUES (%s, %s);"
-        val = (str(message.channel.id), 1)
-        mycursor.execute(sql, val, multi=True)
-        mydb.commit()
-        await message.channel.send("Successfully made this channel a conversating channel.")
+        mycursor.execute("SELECT * FROM allowed_channels WHERE channel_id = " + str(message.channel.id))
+        myresult = mycursor.fetchone()
+        if(myresult == None):
+            sql = "INSERT INTO allowed_channels (channel_id, allowed) VALUES (%s, %s)"
+            val = (str(message.channel.id), 1)
+            mycursor.execute(sql, val)
+            mydb.commit()
+            await message.channel.send("Successfully made this channel a conversating channel. If I react with \"🧠\" (if I have permissions), that means I don't know what that phrase is. If you see it, please run the `qt!addword` command. \n\nTo use it, type `qt!addword <original phrase>; <response>`. Make sure there is a space between the semicolon and the new reply.\n**Example:** `qt!addword How's the weather?; Very sunny!`")
+        else:
+            await message.channel.send("Sorry, this is already a conversating channel. If you would like to remove it as a conversating channel, run the `qt!remove` command.")
         return
+
+
     if(msg == prefix + "remove"):
         mycursor.execute("UPDATE allowed_channels SET allowed = 0 WHERE channel_id = " + str(message.channel.id))
         mydb.commit()
         await message.channel.send("Successfully made this a non-conversating channel.")
+        return
+
+
+    if(msg == prefix + "init"):
+        if(str(message.author.id) != config.get("owner_id")):
+            return
+        else:
+            await message.channel.send("**ONLY USE THIS COMMAND ONCE PER INITIALIZATION!**\n\nIf you deleted the messages table and readded it, then you may run this command again.")
+            mycursor.execute("INSERT INTO messages (sentences, replies) VALUES (\"hello\", \"hey, hi, hello, what's up\")")
+            mycursor.execute("INSERT INTO messages (sentences, replies) VALUES (\"hi\", \"hey, hi, hello, what's up\")")
+            mycursor.execute("INSERT INTO messages (sentences, replies) VALUES (\"hey\", \"hey, hi, hello, what's up\")")
+            mycursor.execute("INSERT INTO messages (sentences, replies) VALUES (\"what's up\", \"hey, hi, hello, what's up\")")
+            mycursor.execute("INSERT INTO messages (sentences, replies) VALUES (\"what's your name\", \"charles, mary, ymir, henrick\")")
+            mydb.commit()
+    if(msg.startswith(prefix + "addphrase")):
+        word = msg.split("; ")
+        word[0] = word[0].replace("qt!addphrase ", "")
+        mycursor.execute("SELECT * FROM messages WHERE sentences = '" + str(filter_message(word[0])) + "'")
+        myresult = mycursor.fetchone()
+        if(myresult != None):
+            if(word[1] not in myresult[2]):
+                mycursor.execute("UPDATE messages SET replies = '" + myresult[2] + ", " + filter_message(word[1]) + "' WHERE sentences = '" + filter_message(word[0]) + "'")
+                mydb.commit()
+                return;
+            else:
+                await message.channel.send("That reply is already added to the list of replies for that word.")
+                return;
+        else:
+            sql = "INSERT INTO messages (sentences, replies) VALUES (%s, %s)"
+            val = (filter_message(word[0]), filter_message(word[1]))
+            mycursor.execute(sql, val)
+            mydb.commit()
+            return;
+        return;
+
+    mycursor.execute("SELECT * FROM allowed_channels WHERE channel_id = " + str(message.channel.id))
+    myresult = mycursor.fetchone()
+    if(myresult != None):
+        mycursor.execute("SELECT * FROM messages WHERE sentences = '" + filter_message(msg) + "'")
+        myresult = mycursor.fetchone()
+        if(myresult != None):
+            replies_to_use = myresult[2].split(", ")
+            await message.channel.send(random.choice(replies_to_use))
+        else:
+            await message.add_reaction("🧠")
+    else:
         return
 client.run(config.get("token"))
