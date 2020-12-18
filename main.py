0b65e673c7cbb5d00917ed3b923a5fcba570d90e
NateTheCarrot <34255388+NateTheCarrot@users.noreply.github.com>
2020-12-18 08:20:54 -0500

    Fixed a bug with checking if a channel is allowed

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ async def on_message(message):
         await message.channel.send(f"**Commands:**\n__{prefix}add__ - Allows the channel the command is used in to participate in the bot (have the bot conversate)\n__{prefix}remove__ - Disallows the bot to conversate in the channel the command is used in.\n__{prefix}addphrase <original phrase>; <response>__ - Lets you add a new phrase to the bot. (Example: `{prefix}addphrase How's the weather?; Very sunny!`)\n__{prefix}init__ - Initializes the database by adding a few premade sentences and replies. Should only be run once! Can only be run by the bot owner.")
     mycursor.execute("SELECT * FROM allowed_channels WHERE channel_id = " + str(message.channel.id))
     myresult = mycursor.fetchone()
-    if(myresult != None): # If it knows the word
+    if(myresult[2] != 0): # If it can go in the channel
         mycursor.execute("SELECT * FROM messages WHERE sentences = '" + filter_message(msg) + "'")
         myresult = mycursor.fetchone()
         if(myresult != None):
