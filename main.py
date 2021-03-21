845180cdbc04eaca60b9818a9851e1e19b5da903
NateTheCarrot <34255388+NateTheCarrot@users.noreply.github.com>
2021-03-21 10:29:39 -0400

    Check if the user should be able to add and remove a conversating channel

--- main.py
+++ main.py
@@ -34,6 +34,8 @@ log_channel = None
 
 client = discord.Client()
 
+owner_id = config.get("owner_id")
+
 def check_if_blacklisted(user):
     """Checks if a user is blacklisted based on blacklisted_users.txt
     
@@ -80,31 +82,37 @@ async def on_message(message):
         return # Return will always be used to make sure to not continue in the code, as it isn't needed.
     msg = message.content.lower()
     if(msg == prefix + "add"):
-        mycursor.execute("SELECT * FROM allowed_channels WHERE channel_id = " + str(message.channel.id))
-        myresult = mycursor.fetchone()
-        if(myresult == None): # Essentially saying "if the channel isn't in the database"
-            sql = "INSERT INTO allowed_channels (channel_id, allowed) VALUES (%s, %s)"
-            val = (str(message.channel.id), 1)
-            mycursor.execute(sql, val)
-            mydb.commit() # Push the changes to the database
-            # Also, f strings help a lot with not having to use + prefix + all the time.
-            await message.channel.send(f"Successfully made this channel a conversating channel. If I react with \"🧠\" (if I have permissions), that means I don't know what that phrase is. If you see it, please run the `{prefix}addphrase` command. \n\nTo use it, type `{prefix}addphrase <original phrase>; <response>`. Make sure there is a space between the semicolon and the new reply.\n**Example:** `{prefix}addphrase How's the weather?; Very sunny!`")
-        elif(myresult[2] == 1):
-            await message.channel.send(f"Sorry, this is already a conversating channel. If you would like to remove it as a conversating channel, run the `{prefix}remove` command.")
+        if message.author.guild_permissions.manage_channels:
+            mycursor.execute("SELECT * FROM allowed_channels WHERE channel_id = " + str(message.channel.id))
+            myresult = mycursor.fetchone()
+            if(myresult == None): # Essentially saying "if the channel isn't in the database"
+                sql = "INSERT INTO allowed_channels (channel_id, allowed) VALUES (%s, %s)"
+                val = (str(message.channel.id), 1)
+                mycursor.execute(sql, val)
+                mydb.commit() # Push the changes to the database
+                # Also, f strings help a lot with not having to use + prefix + all the time.
+                await message.channel.send(f"Successfully made this channel a conversating channel. If I react with \"🧠\" (if I have permissions), that means I don't know what that phrase is. If you see it, please run the `{prefix}addphrase` command. \n\nTo use it, type `{prefix}addphrase <original phrase>; <response>`. Make sure there is a space between the semicolon and the new reply.\n**Example:** `{prefix}addphrase How's the weather?; Very sunny!`")
+            elif(myresult[2] == 1):
+                await message.channel.send(f"Sorry, this is already a conversating channel. If you would like to remove it as a conversating channel, run the `{prefix}remove` command.")
+                return
+            else:
+                await message.channel.send(f"Successfully made this channel a conversating channel. If I react with \"🧠\" (if I have permissions), that means I don't know what that phrase is. If you see it, please run the `qtaddphrase` command. \n\nTo use it, type `{prefix}addphrase <original phrase>; <response>`. Make sure there is a space between the semicolon and the new reply.\n**Example:** `{prefix}addphrase How's the weather?; Very sunny!`")
+                mycursor.execute("UPDATE allowed_channels SET allowed = 1 WHERE channel_id = " + str(message.channel.id))
+                mydb.commit()
+                return
             return
         else:
-            await message.channel.send(f"Successfully made this channel a conversating channel. If I react with \"🧠\" (if I have permissions), that means I don't know what that phrase is. If you see it, please run the `qtaddphrase` command. \n\nTo use it, type `{prefix}addphrase <original phrase>; <response>`. Make sure there is a space between the semicolon and the new reply.\n**Example:** `{prefix}addphrase How's the weather?; Very sunny!`")
-            mycursor.execute("UPDATE allowed_channels SET allowed = 1 WHERE channel_id = " + str(message.channel.id))
-            mydb.commit()
-            return
-        return
+            await message.channel.send(f"Sorry, you need the `MANAGE_CHANNELS` permission to do that. If you believe this is a mistake, please contact <@{owner_id}>.")
 
 
     if(msg == prefix + "remove"):
-        mycursor.execute("UPDATE allowed_channels SET allowed = 0 WHERE channel_id = " + str(message.channel.id)) # Does not return an error if the channel doesn't exist in the database.
-        mydb.commit()
-        await message.channel.send("Successfully made this a non-conversating channel.")
-        return
+        if message.author.guild_permissions.manage_channels:
+            mycursor.execute("UPDATE allowed_channels SET allowed = 0 WHERE channel_id = " + str(message.channel.id)) # Does not return an error if the channel doesn't exist in the database.
+            mydb.commit()
+            await message.channel.send("Successfully made this a non-conversating channel.")
+            return
+        else:
+            await message.channel.send(f"Sorry, you need the `MANAGE_CHANNELS` permission to do that. If you believe this is a mistake, please contact <@{owner_id}>.")
 
 
     if(msg == prefix + "init"):
